53c729ab0d62e2936baf42e7929ff2627f3d3c30
TranGiaPhuoc <tgp780@gmail.com>
2020-10-30 23:26:30 +0700

    Tuan 09

---
+++ Bai Tap 07 ( SQ LITE )/app/src/main/java/com/example/bai_tap_07/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.bai_tap_07 ;
+
+import android.os.Bundle ;
+import android.view.View ;
+import android.widget.Button ;
+import android.widget.EditText ;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity ;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button B_ADD ;
+    private Button B_DELETE ;
+    private Button B_EDIT ;
+    private EditText ET_ID ;
+    private EditText ET_NAME ;
+    private EditText ET_CHEMISTRY ;
+    private EditText ET_MATH ;
+    private EditText ET_PHYSICS ;
+    private My_Database MY_DATABASE ;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState ) ;
+        setContentView( R.layout.activity_main ) ;
+        initListener() ;
+        initView() ;
+        MY_DATABASE = new My_Database( this ) ;
+    }
+
+    private void initListener()
+    {
+        B_ADD.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View VIEW )
+            {
+                String CHEMISTRY = ET_CHEMISTRY.getText().toString() ;
+                String MATH = ET_MATH.getText().toString() ;
+                String NAME = ET_NAME.getText().toString() ;
+                String PHYSICS = ET_PHYSICS.getText().toString() ;
+                boolean BOOLEAN = MY_DATABASE.INSERT( NAME , CHEMISTRY , PHYSICS , MATH ) ;
+                if ( BOOLEAN )
+                    Toast.makeText( MainActivity.this , "INSERTED" , Toast.LENGTH_LONG ).show() ;
+                else
+                    Toast.makeText( MainActivity.this , "INSERT FAIL" , Toast.LENGTH_LONG ).show() ;
+            }
+        }) ;
+        B_DELETE.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View VIEW )
+            {
+                String ID = ET_ID.getText().toString() ;
+                boolean BOOLEAN = MY_DATABASE.DELETE( ID ) ;
+                if ( BOOLEAN )
+                    Toast.makeText( MainActivity.this , "DELETED" , Toast.LENGTH_LONG ).show() ;
+                else
+                    Toast.makeText( MainActivity.this , "DELETE FAIL" , Toast.LENGTH_LONG ).show() ;
+            }
+        }) ;
+        B_EDIT.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View VIEW )
+            {
+                String CHEMISTRY = ET_CHEMISTRY.getText().toString() ;
+                String ID = ET_ID.getText().toString() ;
+                String MATH = ET_MATH.getText().toString() ;
+                String NAME = ET_NAME.getText().toString() ;
+                String PHYSICS = ET_PHYSICS.getText().toString() ;
+                boolean BOOLEAN = MY_DATABASE.UPDATE( ID , NAME , CHEMISTRY , PHYSICS , MATH ) ;
+                if ( BOOLEAN )
+                    Toast.makeText( MainActivity.this , "UPDATED" , Toast.LENGTH_LONG ).show() ;
+                else
+                    Toast.makeText( MainActivity.this , "UPDATE FAIL" , Toast.LENGTH_LONG ).show() ;
+            }
+        }) ;
+    }
+
+    private void initView()
+    {
+        B_ADD = ( Button ) findViewById( R.id.B_ADD ) ;
+        B_DELETE = ( Button ) findViewById( R.id.B_DELETE ) ;
+        B_EDIT = ( Button ) findViewById( R.id.B_EDIT ) ;
+        ET_CHEMISTRY = ( EditText ) findViewById( R.id.ET_CHEMISTRY ) ;
+        ET_ID = ( EditText ) findViewById( R.id.ET_ID ) ;
+        ET_MATH = ( EditText ) findViewById( R.id.ET_MATH ) ;
+        ET_NAME = ( EditText ) findViewById( R.id.ET_NAME ) ;
+        ET_PHYSICS = ( EditText ) findViewById( R.id.ET_PHYSICS ) ;
+    }
+}
---
+++ Bai Tap 07 ( SQ LITE )/app/src/main/java/com/example/bai_tap_07/My_Database.java
@@ -0,0 +1,83 @@
+package com.example.bai_tap_07 ;
+
+import android.content.ContentValues ;
+import android.content.Context ;
+import android.database.sqlite.SQLiteDatabase ;
+import android.database.sqlite.SQLiteOpenHelper ;
+
+public class My_Database extends SQLiteOpenHelper
+{
+    private static final String DATABASE_NAME = "Database.DB" ;
+    private static final String TABLE_NAME = "Sinh_Vien" ;
+    private static final String LINE_01 = "ID" ;
+    private static final String LINE_02 = "NAME" ;
+    private static final String LINE_03 = "CHEMISTRY" ;
+    private static final String LINE_04 = "PHYSICS" ;
+    private static final String LINE_05 = "MATH" ;
+
+    public My_Database( Context CONTEXT )
+    {
+        super ( CONTEXT , DATABASE_NAME , null , 1 ) ;
+    }
+
+    @Override
+    public void onCreate( SQLiteDatabase SQ_LITE_DATABASE )
+    {
+        SQ_LITE_DATABASE.execSQL( "CREATE TABLE " + TABLE_NAME + " (" + LINE_01 + " INTEGER PRIMARY KEY AUTOINCREMENT , " +
+               LINE_02 + " TEXT , " + LINE_03 + " INTEGER , " + LINE_04 + " INTEGER , " + LINE_05 + " INTEGER )") ;
+    }
+
+    @Override
+    public void onUpgrade( SQLiteDatabase SQ_LITE_DATABASE , int I_01 , int I_02 )
+    {
+        SQ_LITE_DATABASE.execSQL( "DROP TABLE IF EXISTS " + TABLE_NAME  ) ;
+        onCreate( SQ_LITE_DATABASE ) ;
+    }
+
+    public boolean DELETE( String ID )
+    {
+        SQLiteDatabase DATABASE = this.getWritableDatabase() ;
+        long RESULTS = DATABASE.delete( TABLE_NAME , LINE_01 + " = ?" , new String[] { ID }) ;
+        if ( RESULTS == -1 )
+            return false ;
+        else
+        {
+            return true ;
+        }
+    }
+
+    public boolean INSERT( String NAME , String CHEMISTRY , String PHYSICS , String MATH )
+    {
+        SQLiteDatabase DATABASE = this.getWritableDatabase() ;
+        ContentValues VALUES = new ContentValues() ;
+        VALUES.put( LINE_02 , NAME ) ;
+        VALUES.put( LINE_03 , CHEMISTRY ) ;
+        VALUES.put( LINE_04 , PHYSICS ) ;
+        VALUES.put( LINE_05 , MATH ) ;
+        long RESULTS = DATABASE.insert( TABLE_NAME , null , VALUES ) ;
+        if ( RESULTS == -1 )
+            return false ;
+        else
+        {
+            return true ;
+        }
+    }
+
+    public boolean UPDATE( String ID , String NAME , String CHEMISTRY , String PHYSICS , String MATH )
+    {
+        SQLiteDatabase DATABASE = this.getWritableDatabase() ;
+        ContentValues VALUES = new ContentValues() ;
+        VALUES.put( LINE_01 , ID ) ;
+        VALUES.put( LINE_02 , NAME ) ;
+        VALUES.put( LINE_03 , CHEMISTRY ) ;
+        VALUES.put( LINE_04 , PHYSICS ) ;
+        VALUES.put( LINE_05 , MATH ) ;
+        long RESULTS = DATABASE.update( TABLE_NAME , VALUES , LINE_01 + " = ?" , new String[] { ID }) ;
+        if ( RESULTS == -1 )
+            return false ;
+        else
+        {
+            return true ;
+        }
+    }
+}
